935d9ac3c3133d927138f274df0127ebb549870f
Tim Jones <tdjones74021@yahoo.com>
2017-01-18 15:26:25 -0600

    Add routes to 'routes/api.php' for JWT authentication.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+// Routes for roles and permissions
+Route::post('role', 'JwtAuthenticateController@createRole');
+Route::post('permission', 'JwtAuthenticateController@createPermission');
+Route::post('assign-role', 'JwtAuthenticateController@assignRole');
+Route::post('attach-permission', 'JwtAuthenticateController@attachPermission');
+
+Route::group(['prefix' => 'api', 'middleware' => ['ability:admin,create-users']], function() {
+	Route::get('users', 'JwtAuthenticateController@index');
+});
+
+Route::post('authenticate', 'JwtAuthenticateController@authenticate');
